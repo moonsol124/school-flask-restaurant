a02dbd9ea936edbeccdd3b3568cb3dc749bc767d
solmoon <moonsol124@gmail.com>
2022-10-18 14:27:09 +0200

    update

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/menu")
+def menu():
+    return render_template('menu.html')
+
+@app.route("/menu/pasta")
+def pasta_menu():
+    return render_template('menu_pasta.html')
+
+@app.route("/menu/drinks")
+def drinks_menu():
+    return render_template('menu_drinks.html')
 
 @app.route('/register')
 def register():
     return render_template('register_form.html')
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
